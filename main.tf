1465ef85a9601f14aeb0773da7b4b1f716f29f68
rogmanster <rogman@gmail.com>
2022-10-12 17:09:02 -0700

    ubuntu ami

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "rhel_ami" {
+/*data "aws_ami" "rhel_ami" {
   most_recent = true
   owners      = ["309956199498"]
 
@@ -6,6 +6,23 @@ data "aws_ami" "rhel_ami" {
     name   = "name"
     values = ["*RHEL-7.3_HVM_GA-*"]
   }
+}*/
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    #values = ["ubuntu/images/hvm-ssd/ubuntu-disco-19.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
 resource "random_id" "name" {
@@ -19,7 +36,8 @@ resource "aws_key_pair" "awskey" {
 
 resource "aws_instance" "ubuntu" {
   count                   = var.instance_count
-  ami                     = data.aws_ami.rhel_ami.id
+  #ami                     = data.aws_ami.rhel_ami.id
+  ami                     = data.aws_ami.ubuntu.id
   instance_type           = var.instance_type
   key_name                = aws_key_pair.awskey.key_name
   vpc_security_group_ids  = var.vpc_security_group_ids
