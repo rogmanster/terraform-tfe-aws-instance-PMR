0a1a06d3d3bfeca217d5f5a9f3218627a1f01bac
rogmanster <rogman@gmail.com>
2022-12-05 09:42:15 -0800

    module update

--- main.tf
+++ main.tf
@@ -1,6 +1,34 @@
-provider "aws" {
-  
+terraform {
+  required_version = ">= 0.11.0"
 }
+
+data "terraform_remote_state" "aws_vpc_prod" {
+  backend = "remote"
+
+  config = {
+    organization = "rogercorp"
+    workspaces = {
+      name = "aws-vpc-prod"
+    }
+  }
+}
+
+data "terraform_remote_state" "aws_security_group" {
+  backend = "remote"
+
+  config = {
+    organization = "rogercorp"
+    workspaces = {
+      name = "aws-security-group-prod"
+    }
+  }
+}
+
+data "aws_key_pair" "example" {
+  key_name           = "rchao-key"
+  include_public_key = true
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -17,25 +45,34 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+provider "aws" {
+}
+
 resource "random_id" "name" {
   byte_length = 4
 }
 
-resource "aws_key_pair" "awskey" {
-  key_name   = "awskwy-${random_id.name.hex}"
-  public_key = tls_private_key.awskey.public_key_openssh
-}
-
 resource "aws_instance" "ubuntu" {
   count                   = var.instance_count
-  #ami                     = data.aws_ami.rhel_ami.id
   ami                     = data.aws_ami.ubuntu.id
   instance_type           = var.instance_type
-  key_name                = aws_key_pair.awskey.key_name
+  key_name                = data.aws_key_pair.example.key_name
+  vpc_security_group_ids  = [data.terraform_remote_state.aws_security_group.outputs.security_group_id]
+  subnet_id               = data.terraform_remote_state.aws_vpc_prod.outputs.public_subnets[0]
 
   tags = {
-    Name        = var.name
-    TTL         = var.ttl
-    Description = "This branch updated v6"
+    name        = var.name
+    ttl         = var.ttl
+    env         = var.env
+    Description = "This branch updated v1.0.3"
+  }
+
+  //requires Terraform v1.2 or higher
+  //https://developer.hashicorp.com/terraform/language/expressions/custom-conditions#self-object
+  lifecycle {
+    postcondition {
+      condition     = self.instance_state == "running"
+      error_message = "EC2 instance must be running."
+    }
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,6 @@
 #Instance
-variable "instance_count" {
-  description = "The number of EC2 Instances"
-  default = 1
-}
-variable "instance_type" {
-  description = "The type of EC2 Instances"
-  default = "t2.micro"
-}
-variable "name" {
-  description = "The name of EC2 Instances"
-  default = "rchao"
-}
-variable "ttl" {
-  description = "A TTL for when to terminate"
-  default = "1"
-}
+variable "instance_count" {}
+variable "instance_type" {}
+variable "name" {}
+variable "env" {}
+variable "ttl" {}
